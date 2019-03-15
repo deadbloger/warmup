ce59d85e72d4e05f773e82ca55ad2fe097cad198
Mikita Plotnikau <deadbloger@gmail.com>
2019-03-15 12:52:58 +0300

    add warmup task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function warmup(temperature) {
-  // your implementation here
+module.exports = function warmup(fahrenheit) {
+
+    if (typeof fahrenheit !== 'number') {
+        throw new TypeError('Введите числовое значение');
+    }
+
+    return fahrenheit * 1.8 + 32;
+
+
 };
